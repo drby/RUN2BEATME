10a5beecf9da2d23a06166c2466eb3bf04fd9671
Tom Darby <tomd.57@live.com>
2019-11-26 18:58:02 +0100

    map displayed

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,4 +1,16 @@
 class RacesController < ApplicationController
+
+  def index
+    @users = User.geocoded # returns users with coordinates
+
+    @users = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
+  end
+
   def show
     @race = Race.find(params[:id])
     @runs = Run.where(race_id: @race)
