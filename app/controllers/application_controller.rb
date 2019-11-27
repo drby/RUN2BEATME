f9114e816d62c5bffd6a0b4a83fd4776bcd29ecf
Yann Caperan <yann.caperan@gmail.com>
2019-11-27 16:48:55 +0100

    Delete Navbar for Home/sign_In_Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  layout :layout_by_resource
+
   before_action :authenticate_user!
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || challenges_path
   end
+
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "home_layout"
+    else
+      "application"
+    end
+  end
+
 end
 
