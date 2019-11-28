17bb737af1b7432005b55c7b2b10364c7fa65e1e
Mickael Durand <micdurand@gmail.com>
2019-11-28 15:17:24 +0100

    update wallet when race finished ok

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -19,14 +19,23 @@ class RacesController < ApplicationController
 
   def update
     @race = Race.find(params[:id])
+    @challenge = @race.challenge
+
     if @race.pending? # si le bouton start est clique et que la race est a pending
       @race.update(progress: 1, start_at: DateTime.now) # alors on update a in_progress et on lui affecte le temps du départ
       redirect_to race_path(@race)
     elsif @race.in_progress? # si le bouton finish est clique et que la race est a in_progress
       @race.update(progress: 2) # on passe le statut a finished
+
       @race.runs.first.update(state: 1) # on dit que c'est le 1er user qui a gagne
       @race.runs.first.update(finished_at: DateTime.now) # on lui affecte le temps d'arrive
-      @race.runs.last.update(state: 2) # on dit que c'est le 2eme user qui a perd
+      w1 = @race.runs.first.user.wallet
+      @race.runs.first.user.update(wallet: (w1 + @challenge.bet)) # on lui credite son compte le temps d'arrive
+
+      @race.runs.last.update(state: 2) # on dit que c'est le 2eme user qui a perdu
+      w2 = @race.runs.last.user.wallet
+      @race.runs.last.user.update(wallet: (w2 - @challenge.bet)) # on lui debite son compte le temps d'arrive
+
 
       redirect_to race_path(@race)
     end
