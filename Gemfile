6b0e3176d826653de57624c79867eaf686bc79cf
Tom Darby <tomd.57@live.com>
2019-11-28 17:37:36 +0100

    iti

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'faker'
 gem 'geocoder'
+gem "mapbox-sdk"
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -4,20 +4,24 @@ class RacesController < ApplicationController
 
   end
 
+  def update
+    raise
+  end
+
   def show
     @race = Race.find(params[:id])
-    @runs = Run.where(race_id: @race.id)
-    @runs = @runs.select { |run| run.user_id != current_user.id }
+    # @runs = Run.where(race_id: @race.id)
+    @run = Run.find(5)
+    @runs = Run.where(race_id: 3)
+    # @runs = @runs.select { |run| run.user_id != current_user.id }
+    # @runs_pos = Run.geocoded # returns users with coordinates
 
-    @runs_pos = Run.geocoded # returns users with coordinates
-
-    @markers = @runs_pos.map do |run|
-      {
-        start_lat: run.start_latitude,
-        start_lng: run.start_longitude,
-        end_lat: run.finish_latitude,
-        end_lng: run.finish_latitude
+    @markers = {
+        start_lat: @run.start_latitude,
+        start_lng: @run.start_longitude,
+        end_lat: @run.finish_latitude,
+        end_lng: @run.finish_longitude
       }
-    end
+
   end
 end
