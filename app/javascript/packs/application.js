9e8baafc009c76cd230aedf4a1a60936755456c2
Mickael Durand <micdurand@gmail.com>
2019-11-29 14:20:29 +0100

    position runs ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,6 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 
 import { initMapbox } from '../plugins/init_mapbox';
 
-initMapbox();
-
 const counterElement = document.querySelector("#counter");
 const CheckReload = (() => {
   let counter = 0;
@@ -19,7 +17,26 @@ const refreshId = setInterval( () => {
     counterElement.innerText = `${ 15 - properID} s`
     if (properID >= 15) {
       clearInterval(refreshId);
-      document.getElementById("btn-launch-race").click();
+      navigator.geolocation.getCurrentPosition((data) =>  {
+      const lat = data.coords.latitude;
+      const long = data.coords.longitude;
+      const input1 = document.createElement("input");
+      const input2 = document.createElement("input");
+      input1.setAttribute("name", "lat");
+      input1.setAttribute("value", lat);
+      input1.setAttribute("type", "hidden");
+      input2.setAttribute("name", "long");
+      input2.setAttribute("value", long);
+      input2.setAttribute("type", "hidden");
+
+      const form = document.querySelector(".simple_form")
+      form.prepend(input1)
+      form.prepend(input2)
+
+      console.log(form);
+      form.submit();
+    });
+
     }
   },
   1000
@@ -27,3 +44,5 @@ const refreshId = setInterval( () => {
 }
 
 
+
+initMapbox();
