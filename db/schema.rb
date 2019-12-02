67f7bc8e06d58928f029c96a6ab19e0529c5f339
Mickael Durand <micdurand@gmail.com>
2019-12-02 10:54:28 +0100

    now a user can't be its own opponent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_160136) do
+ActiveRecord::Schema.define(version: 2019_11_27_155112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2019_11_28_160136) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
