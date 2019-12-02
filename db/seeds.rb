e4e53fd954fab4db5dc823493c38d6c90cae5f2c
Yann Caperan <yann.caperan@gmail.com>
2019-12-02 11:53:57 +0100

    Added Seed & images & Adjust Navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'faker'
 puts "Destroy everything"
 User.destroy_all
 Run.destroy_all
@@ -7,14 +6,13 @@ Challenge.destroy_all
 puts "Finished"
 
 
-puts "Create Users with Faker"
- User.create(username: 'Tomy', email: 'tomy@gmail.com', password: 'azerty', wallet: 100)
-20.times do
-  User.create(username: Faker::Internet.username, email: Faker::Internet.email, password: 'azerty', wallet: rand(0..100))
-end
+puts "Creating 4 Users..."
+ User.create(username: 'tom_y', email: 'tom.drby@gmail.com', password: 'azerty', wallet: 100, photo: "tom.jpg")
+ User.create(username: 'mick_ado', email: 'mick.a@gmail.com', password: 'azerty', wallet: 200, photo: "mika.jpg")
+ User.create(username: 'yann_os', email: 'yann.os@gmail.com', password: 'azerty', wallet: 150, photo: "yann.jpg")
 
 
-puts "Create Challenge"
+puts "Creating 10 Challenges ..."
   Challenge.create(category: 0, distance: 500, bet: 5)
   Challenge.create(category: 0, distance: 500, bet: 10)
   Challenge.create(category: 0, distance: 700, bet: 10)
@@ -25,3 +23,5 @@ puts "Create Challenge"
   Challenge.create(category: 0, distance: 1000, bet: 40)
   Challenge.create(category: 0, distance: 1000, bet: 50)
   Challenge.create(category: 0, distance: 2200, bet: 100)
+
+puts "Finished"
