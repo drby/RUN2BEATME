d632249835260b982b362a4a95aab209a6c08d55
Yann Caperan <yann.caperan@gmail.com>
2019-12-03 15:42:02 +0100

    permit params for wallet, email, password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,25 @@
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   layout :layout_by_resource
 
-  before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || challenges_path
   end
 
 
+  def configure_permitted_parameters
+    update_attrs = [:wallet, :email, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  end
+
+
   private
+
 
   def layout_by_resource
     if devise_controller?
